594fe2e5bd6aa723fb9658c50c4d483a1bffd3b8
Ashish <ashishdevkota03@gmail.com>
2021-06-01 12:31:53 +0200

    files added

---
+++ app.js
@@ -0,0 +1,6 @@
+   // toggle menu 
+   var handburger=document.getElementsByClassName('handburger')[0]
+   handburger.addEventListener('click',()=>{
+      var menuItems=document.getElementsByClassName('menu-items')[0];
+      menuItems.classList.toggle('active')
+   })
